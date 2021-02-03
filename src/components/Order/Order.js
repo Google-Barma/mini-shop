3676b1bcc7cfaad6856932dcd6c2daeb5c03294a
Vadym <vad.evlanov@gmail.com>
2021-02-03 18:42:24 +0200

    add selectors

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,15 +1,28 @@
 import { NavLink } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import OrderList from '../OrderList/OrdersList';
 import { isOpenOrder } from '../../redux/product/product-action';
 import s from './Order.module.css';
+import {
+  getTotalAmount,
+  getTotalPrice,
+} from '../../redux/product/product-selectors';
+import toLocale from '../../service/toLocale';
 
 export default function Order() {
   const dispatch = useDispatch();
+  const totalPrice = useSelector(getTotalPrice);
+  const totalAmount = useSelector(getTotalAmount);
 
   return (
     <div className={s.orderWrap}>
       <OrderList />
+
+      <div>
+        <span className={s.total}>Total: {toLocale(totalAmount)}</span>
+        <span className={s.price}>Price: {toLocale(totalPrice)}</span>
+      </div>
+
       <div className={s.controls}>
         <NavLink
           type="button"
--- src/redux/product/product-selectors.js
+++ src/redux/product/product-selectors.js
@@ -1,7 +1,20 @@
 // import { createSelector } from '@reduxjs/toolkit';
 
+import { createSelector } from '@reduxjs/toolkit';
+
 export const getProducts = state => state.products;
 
 export const getOrders = state => state.orderList.orders;
 
 export const getIsOpen = state => state.orderList.isOpen;
+
+export const getTotalAmount = createSelector([getOrders], orders =>
+  orders.reduce((total, order) => total + order.amount, 0),
+);
+
+export const getTotalPrice = createSelector([getOrders], orders =>
+  orders.reduce(
+    (total, order) => total + order.price * order.amount + Number(order.smth),
+    0,
+  ),
+);
